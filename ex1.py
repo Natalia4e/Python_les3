cbc955bf3fd45764e1d72a1e7ed38ae1470b4324
Natalia4e <110606894+Natalia4e@users.noreply.github.com>
2022-08-20 15:00:46 +0300

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,12 @@
+num_translate = {'one': 'один',
+               'two': 'два',
+               'three': 'три',
+               'four ': 'четыре',
+              'five': 'пять',
+              'six': 'шесть',
+              'seven': 'семь',
+              'eight': 'восемь',
+              'nine':'девять',
+              'ten': 'десять'}
+
+print(num_translate.get('one'))
